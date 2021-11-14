8fc2621a32a1bd6a20af7f9702697f8c0d3fed4a
Edward <jaysongraphics@gmail.com>
2021-11-13 19:33:01 -0500

    updates

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,10 +24,6 @@ class UsersController < ApplicationController
   
   private
 
-    def set_user
-      @user = User.find(params[:id])
-    end
-
     def user_params
       params.permit(:payer, :points, :timestamps)
     end
